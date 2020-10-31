c392cfdc3ff848894af09e5c83c26a9fff4fb485
Achmad Ichsan <ichsanachmad33@gmail.com>
2020-10-31 11:40:42 +0700

    remove unused import

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:progress_dialog/progress_dialog.dart';
 import 'package:todo_flutter/bloc/todo/todo_bloc.dart';
 import 'package:todo_flutter/bloc/todo/todo_event.dart';
 import 'package:todo_flutter/bloc/todo/todo_state.dart';
